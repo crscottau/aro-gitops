8a25f2c5954fc01669fd8d9523870bdad52f236c
Craig Scott <crscott@redhat.com>
2024-11-04 16:26:59 +1100

    Add bootstrap bash and powershell scripts

---
+++ bootstrap/bootstrap-openshift-gitops.ps1
@@ -0,0 +1,42 @@
+# Set environment variables
+$KUBECONFIG = "C:\OpenShift\kubeconfig"
+$BYPASS_TLS = "true"
+
+# Define OpenShift command with kubeconfig and bypass TLS options
+$OC = "C:\OpenShift\oc --kubeconfig $KUBECONFIG --insecure-skip-tls-verify=$BYPASS_TLS"
+
+# Authenticate to the cluster
+Write-Output "Authenticating to the cluster as ""$($(& $OC whoami))"""
+
+# Apply the Catalogue Source and ICSP files
+Write-Output "Applying the Catalogue Source and ICSP files:"
+& $OC patch OperatorHub cluster --type json --patch '[{"op": "add", "path": "/spec/disableAllDefaultSources", "value": true}]'
+& $OC apply -f catalogSource-cs-redhat-operator-index.yaml
+& $OC apply -f imageContentSourcePolicy.yaml
+Write-Output ""
+
+# Prepare OpenShift GitOps to run with ClusterAdmin privileges
+Write-Output "Preparing OpenShift GitOps to run with ClusterAdmin privileges"
+& $OC apply -f crb-cluster-admin-openshift-gitops-argocd-application-controller.yaml
+Write-Output ""
+
+# Install the operator
+Write-Output "Installing the operator"
+& $OC apply -f gitops-subscription.yaml
+Write-Output ""
+
+# Wait for the operator to install
+Write-Output "Waiting for the operator to install"
+while ($true) {
+    $OperatorName = & oc -n openshift-gitops get csv -o json | ConvertFrom-Json | ForEach-Object { $_.items | Where-Object { $_.metadata.name -match "openshift-gitops" } | Select-Object -ExpandProperty "metadata" | Select-Object -ExpandProperty "name" }
+    $OperatorStatus = & oc -n openshift-gitops get csv $OperatorName -o jsonpath='{.status.phase}'
+    Write-Output $OperatorStatus
+    if ($OperatorStatus -eq "Succeeded") {
+        break
+    }
+    Start-Sleep -Seconds 5
+}
+
+Write-Output ""
+Write-Output "Creating the 'Application of Applications'"
+& $OC apply -f gitops-application.yaml
---
+++ bootstrap/bootstrap-openshift-gitops.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+export KUBECONFIG=/home/crscott/tmp/kubeconfig
+export BYPASS_TLS=true
+
+OC="oc --kubeconfig ${KUBECONFIG} --insecure-skip-tls-verify=${BYPASS_TLS}" 
+
+echo Authenticating to the cluster as \"$(${OC} whoami)\"
+
+echo Applying the Catalogue Source and ICSP files:
+${OC} patch OperatorHub cluster --type json --patch '[{"op": "add", "path": "/spec/disableAllDefaultSources", "value": true}]'
+${OC} apply -f catalogSource-cs-redhat-operator-index.yaml                             
+${OC} apply -f imageContentSourcePolicy.yaml
+echo ""
+
+echo Preparing OpenShift GitOps to run with ClusterAdmin privileges
+${OC} apply -f crb-cluster-admin-openshift-gitops-argocd-application-controller.yaml
+echo ""
+
+echo Installing the operator
+${OC} apply -f gitops-subscription.yaml
+echo ""
+
+echo Wait for the operator to install
+while true
+do
+  OPERATOR_NAME=$(oc -n openshift-gitops get csv -o json | jq -r '.items[] | select(.metadata.name|contains ("openshift-gitops")) | .metadata.name')
+  OPERATOR_STATUS=$(oc -n openshift-gitops get csv ${OPERATOR_NAME} -o jsonpath='{.status.phase}')
+  echo $OPERATOR_STATUS
+  if [ "$OPERATOR_STATUS" = "Succeeded" ]; then
+    break
+  fi
+done
+
+echo ""
+echo Creating the "Application of Applications"
+${OC} apply -f gitops-application.yaml
